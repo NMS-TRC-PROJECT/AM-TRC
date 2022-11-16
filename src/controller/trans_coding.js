713330269205befe38a1927488717e7d22d2ae78
sanghyun234 <shlee@amuzlab.com>
2022-11-16 17:16:40 +0900

    5 coomit

--- src/controller/trans_coding.js
+++ src/controller/trans_coding.js
@@ -1,28 +1,21 @@
 import { exec } from "child_process";
-import dotenv from "dotenv";
-dotenv.config();
-
-const ffmpeg = process.env.FFMPEG;
+import { ts } from "../modules/trans_coding.js";
 
 export const trans_coding = (req, res) => {
   try {
     const { input, resolution, video_c, audio_c, Kbps_v, output } = req.body;
 
-    let command = `${ffmpeg} -i /home/shlee/${input}`;
-    if (resolution) command += ` -s ${resolution}`;
-    if (video_c) command += ` -c:v ${video_c}`;
-    if (audio_c) command += ` -c:a ${audio_c}`;
-    if (Kbps_v) command += ` -b:v ${Kbps_v}`;
-    command += ` /home/shlee/${output}`;
-
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error.toString()}`);
-        return;
+    exec(
+      ts(input, resolution, video_c, audio_c, Kbps_v, output),
+      (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error.toString()}`);
+          return;
+        }
+        console.log(`stdout: ${stdout.toString()}`);
+        console.log(`stderr: ${stderr.toString()}`);
       }
-      console.log(`stdout: ${stdout.toString()}`);
-      console.log(`stderr: ${stderr.toString()}`);
-    });
+    );
 
     return res.status(201).json({ resultCode: 201, errorString: "" });
   } catch (error) {
